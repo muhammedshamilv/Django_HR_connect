6e67479cb67eba919a79bd45ac0652fab25c9f86
muhammedshamilv <shamil@hamon.in>
2023-12-01 11:31:02 +0530

    model registration

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Designation)
+admin.site.register(Project)
+admin.site.register(TaskLog)
+admin.site.register(ServiceRecord)
+admin.site.register(PromotionHistory)
+admin.site.register(AchievementHistory)
+admin.site.register(UserProject)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class UserModel(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'code'
+    )
+
+
+admin.site.register(User, UserModel)
